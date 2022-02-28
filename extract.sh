d546c44901369f2201eceb870fa32fef6f729fd6
mrellis96 <82981124+mrellis96@users.noreply.github.com>
2022-02-28 16:33:19 +1100

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -8,3 +8,6 @@ while read -r line; do
     out=$(echo $name | cut -f1 -d'_' )
     grep $name "$2" -A1 > $out
 done < "$filename"
+
+#one line
+cat contig.list | while read line; do out=$(echo $line | cut -f1 -d'_'); grep $line trimmed.fasta -A1 > 18S/"$out".18s.fa; done
