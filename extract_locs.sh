18ffd3450d0a9b33acc7306412e50e0a8d8a238d
mrellis96 <82981124+mrellis96@users.noreply.github.com>
2024-12-03 09:54:28 +1100

    extract locations from genbankfile
    extract locations from genbankfile

---
+++ extract_locs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/awk -f
+
+BEGIN {
+    location = "NA";  # Default location value
+}
+
+/^ACCESSION/ {
+    acc = $2;         # Capture the accession number
+    location = "NA";  # Reset location for each accession
+}
+
+/^ {5}source/,/^ {5}\S/ {
+    # Look for /country= or /geo_loc_name= qualifiers
+    if ($0 ~ /\/country=/) {
+        location = substr($0, index($0, "/country=") + 9);  # Start extracting after '/country='
+        gsub(/^"|"$/, "", location);                       # Remove surrounding quotes
+    } else if ($0 ~ /\/geo_loc_name=/) {
+        location = substr($0, index($0, "/geo_loc_name=") + 14);  # Start after '/geo_loc_name='
+        gsub(/^"|"$/, "", location);                              # Remove surrounding quotes
+    }
+}
+
+# When a new accession or end of the block is reached, print the data
+/^ACCESSION/ || /^ {0,4}\/\// {
+    if (acc != "") {
+        print acc, location;  # Print accession and location
+    }
+}
